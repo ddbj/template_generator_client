f04ccf7ab9531d7ba933d001fe9806f4741c1524
oec <oec@Schnauzer.local>
2024-07-10 13:10:05 +0900

    a branch for development added

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [fetch_schema, setSchema] = useState(null);
 
+  const [data, setData] = useState(null)
 
 useEffect(() => {
-    fetch('https://raw.githubusercontent.com/ddbj/template_generator_api/main/src/dev_schemas/ddbj_submission_dev1.json')
+    //fetch('https://raw.githubusercontent.com/ddbj/template_generator_api/main/src/dev_schemas/ddbj_submission_dev1.json')
     //fetch('https://raw.githubusercontent.com/ddbj/template_generator_client/main/schemas/example_schema_minimum_test.json')
+    fetch('test.json')
     .then(res => res.json())
     .then(data => {
         setSchema(data);
@@ -22,10 +24,32 @@ useEffect(() => {
 
 
   const uiSchema = {
-    "division": {
-      'ui:Placeholder': 'Select your division'
+    data_type: {
+      "ui:help": (
+        <div style={{'fontSize':'1rem'}}>See dfast's <a href="https://dfast.ddbj.nig.ac.jp/">original website</a> for more information.</div>
+      )
+    },
+    form3: {
+      "ui:FieldTemplate": CustomFieldTemplate
     }
   };
+
+
+  function CustomFieldTemplate(props) {
+    console.log("props: ", props)
+    const {id, classNames, label, help, required, description, errors, children} = props;
+    return (
+      <div className={classNames}>
+        <label htmlFor={id}>{label}{required ? "*" : null}</label>
+        {description}
+        {children}
+        {errors}
+        {help}
+      </div>
+    );
+  }
+
+
   
   return (
     <div className='app_container'>
@@ -34,6 +58,12 @@ useEffect(() => {
                 schema={fetch_schema} 
                 validator={validator} 
                 uiSchema={uiSchema}
+                CustomFieldTemplate = {CustomFieldTemplate}
+                formData={data}
+                onChange={(e) => {
+                  const { formData } = e
+                  console.log(formData.form2)
+                }}
             >
             </Form>
         :
